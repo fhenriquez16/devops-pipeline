68b587b9da4273a878487ce464be78a071a807e7
Francisco Henriquez <fhenriquez@coexca.cl>
2023-03-16 20:51:26 -0300

    cambios full

---
+++ products/__tests__/index.test.js
@@ -0,0 +1,10 @@
+import supertest from 'supertest'
+import app from '../src/app'
+
+describe('Index route', () => {
+    it('should return an empty object', async () => {
+        const res = await supertest(app).get('/products')
+
+        expect(res.statusCode).toEqual(200)
+    })
+}) 
