f4d7768ec92c1e12b762bb15cf44f3077b8a5a33
Francisco Henriquez <fhenriquez@coexca.cl>
2023-03-20 18:57:25 -0300

    cambios eslint

---
+++ products/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'jest/globals': true 
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	plugins: ['jest'], 
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
